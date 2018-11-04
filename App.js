1826e9a552ab7fa5450eff7ceb4ba64cd52ceb91
Kanta Matsui <kma9879@gmail.com>
2018-11-05 01:22:04 +0900

    chore: https://github.com/teradonburi/learnReactJS/tree/ReactRedux

--- App.js
+++ App.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { load } from './user'
 
+// connectのdecorator
+@connect(
+  // propsに受け取るreducerのstate
+  state => ({
+    users: state.user.users
+  }),
+  // propsに付与するactions
+  { load }
+)
 export default class App extends React.Component {
 
-  render () {
-    return <h1>Hello, world!</h1>
+  componentDidMount() {
+    // user取得APIコールのactionをキックする
+    this.props.load()
   }
 
+  render () {
+    const { users } = this.props
+    // 初回はnullが返ってくる（initialState）、処理完了後に再度結果が返ってくる
+    console.log(users)
+    return (
+      <div>
+          {/* 配列形式で返却されるためmapで展開する */}
+          {users && users.map((user) => {
+            return (
+                // ループで展開する要素には一意なkeyをつける（ReactJSの決まり事）
+                <div key={user.email}>
+                  <img src={user.picture.thumbnail} />
+                  <p>名前:{user.name.first + ' ' + user.name.last}</p>
+                  <p>性別:{user.gender}</p>
+                  <p>email:{user.email}</p>
+                </div>
+            )
+          })}
+      </div>
+    )
+  }
 }
