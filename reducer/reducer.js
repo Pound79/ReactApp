35d0d9b0980e3be6cd6b3b6f649d5263be4b6e4f
Kanta Matsui <kma9879@gmail.com>
2018-11-06 00:33:18 +0900

    chore: https://github.com/teradonburi/learnReactJS/tree/ReduxForm

--- reducer/reducer.js
+++ reducer/reducer.js
@@ -1,7 +1,9 @@
-import { combineReducers } from 'redux'
+import {combineReducers} from 'redux'
+import {reducer as formReducer} from 'redux-form' // as は名前がかぶらないようにインポート時に別名にできる
 
 import user from './user'
 
 export default combineReducers({
-  user,
+    form: formReducer, // 追加
+    user
 })
